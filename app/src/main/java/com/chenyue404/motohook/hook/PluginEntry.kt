3991a2073ab373f4036ddb7f3663b166338ec3df
chenyue404 <601450225@qq.com>
2022-09-18 14:56:17 +0800

    去除电池图标

---
+++ app/src/main/java/com/chenyue404/motohook/hook/PluginEntry.kt
@@ -0,0 +1,17 @@
+package com.chenyue404.motohook.hook
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+/**
+ * Created by chenyue on 2022/9/18 0018.
+ */
+class PluginEntry : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        listOf(
+            BatteryHook()
+        ).forEach {
+            it.handleLoadPackage(lpparam)
+        }
+    }
+}
