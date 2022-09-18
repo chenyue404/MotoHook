3ae6a97c6d0ecbc99b26db105b5d8b05b8b99ce6
chenyue404 <601450225@qq.com>
2022-09-18 17:01:04 +0800

    小窗关键方法

---
+++ app/src/main/java/com/chenyue404/motohook/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.chenyue404.motohook
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by chenyue on 2022/9/18 0018.
+ */
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/chenyue404/motohook/hook/SystemUIHook.kt
+++ app/src/main/java/com/chenyue404/motohook/hook/SystemUIHook.kt
@@ -1,9 +1,8 @@
 package com.chenyue404.motohook.hook
 
-import android.view.View
-import android.widget.ImageView
-import androidx.core.view.isVisible
-import de.robv.android.xposed.*
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers.findAndHookMethod
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
@@ -12,8 +11,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
  */
 class SystemUIHook : IXposedHookLoadPackage {
 
-    private val PACKAGE_NAME = "com.android.systemui"
-    private val TAG = "Moto-systemui-hook-"
+    private val PACKAGE_NAME = "com.motorola.freeform"
+    private val TAG = "Moto-freeform-hook-"
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
 
@@ -26,48 +25,18 @@ class SystemUIHook : IXposedHookLoadPackage {
 
         log("")
 
-        //隐藏电池图标
         findAndHookMethod(
-            "com.android.systemui.BatteryMeterView", classLoader,
-            "scaleBatteryMeterViews",
+            "b.d.a.j.b", classLoader,
+            "c",
+            String::class.java,
             object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    log("com.android.systemui.BatteryMeterView#scaleBatteryMeterViews")
-                    val mBatteryIconView =
-                        XposedHelpers.findField(param.thisObject.javaClass, "mBatteryIconView")
-                            .get(param.thisObject) as ImageView
-                    log("获取mBatteryIconView")
-                    mBatteryIconView.isVisible = false
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    log("b.d.a.j.b#c")
+                    val str = param.args[0] as String
+                    log("str=$str")
                 }
             }
         )
-
-        //长按最近任务按钮打开菜单
-        findAndHookMethod("com.android.systemui.navigationbar.NavigationBar",
-            classLoader,
-            "updateScreenPinningGestures",
-            object : XC_MethodReplacement() {
-                override fun replaceHookedMethod(param: MethodHookParam) {
-                    log("com.android.systemui.navigationbar.NavigationBar#updateScreenPinningGestures")
-                    val mNavigationBarView =
-                        XposedHelpers.findField(param.thisObject.javaClass, "mNavigationBarView")
-                            .get(param.thisObject)
-                    log("获取mNavigationBarView")
-                    val recentButton =
-                        XposedHelpers.callMethod(mNavigationBarView, "getRecentsButton")
-                    log("获取recentButton")
-                    XposedHelpers.callMethod(
-                        recentButton,
-                        "setOnLongClickListener",
-                        object : View.OnLongClickListener {
-                            override fun onLongClick(v: View): Boolean {
-                                log("recentButton长按")
-                                Runtime.getRuntime().exec("input keyevent 82")
-                                return true
-                            }
-                        })
-                }
-            })
     }
 
     private fun log(str: String) {
